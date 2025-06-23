2ec749dbaf2101a4bded5bfd06d0b15d80298ba4
oycyc <yangciouou@gmail.com>
2025-06-23 07:22:33 -0400

    feat: managing aws identity center sso users and groups and assignments and permission sets in terraform

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,19 @@
-output "random_pet_name" {
-  description = "The generated random pet name"
-  value       = random_pet.template.id
+output "users" {
+  value = local.users_map
+}
+
+output "groups" {
+  value = local.groups_map
+}
+
+output "group_memberships" {
+  value = local.group_memberships
+}
+
+output "account_assignments" {
+  value = local.account_assignments
+}
+
+output "permission_sets" {
+  value = local.permission_sets_map
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,46 @@
-variable "length" {
-  description = "The length of the random name"
-  type        = number
-  default     = 2
+variable "users" {
+  description = "List of SSO users"
+  type = list(object({
+    user_name   = string
+    given_name  = string
+    family_name = string
+    email       = string
+  }))
+}
+
+variable "groups" {
+  description = "List of SSO groups"
+  type = list(object({
+    name        = string
+    description = string
+    members     = list(string)
+    assignments = optional(list(object({
+      permission_set = string
+      account_ids    = list(string)
+    })), [])
+  }))
+}
+
+variable "permission_sets" {
+  description = "List of permission sets"
+  type = list(object({
+    name                                = string
+    description                         = string
+    managed_policies                    = optional(list(string), [])
+    session_duration                    = optional(string, "PT12H")
+    inline_policy                       = optional(string, null)
+    relay_state                         = optional(string, null)
+    tags                                = optional(map(string), {})
+    customer_managed_policy_attachments = optional(list(string), [])
+  }))
+}
+
+variable "instance_arn" {
+  description = "SSO instance ARN"
+  type        = string
+}
+
+variable "identity_store_id" {
+  description = "Identity store ID"
+  type        = string
 }
